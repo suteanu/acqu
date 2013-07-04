663bf70fb764ff0413ba4ba39cbfaff65455e30d
Cristina Collicott <Collicott@dal.ca>
2013-07-04 11:51:31 +0200

    Fixed Tagger Energy Issue

--- acqu_user/root/macros/MissingMassTree.C
+++ acqu_user/root/macros/MissingMassTree.C
@@ -93,9 +93,10 @@ void MissingMass2008() {
 
 }
 
+
 void MissingMass2012() {
 
-	Char_t* filename = Form("/work0/cristina/TA2Pi0Compton2012.root");
+	Char_t* filename = Form("/work0/cristina/TA2Pi0Compton2012-373.root");
 	TFile *file1 = new TFile(filename);
 	TTree *tree1 = (TTree*)file1->Get("Pi0ComptonTree");
 
@@ -166,6 +167,98 @@ void MissingMass2012() {
 		}
 	}
 	
+	canvas1 = new TCanvas("canvas1","Missing-Mass-Compton");
+	a2->Add(a0,1);
+	a2->Add(a1,-0.025); //Data2012
+	a2->Draw();
+
+	canvas2 = new TCanvas("canvas2","Missing-Mass-Pi0");
+	b2->Add(b0,1);
+	b2->Add(b1,-0.025); // Data2012
+	b2->Draw();
+
+}
+
+void MissingMassComparePi0() {
+
+	Char_t* filename = Form("/work0/cristina/TA2Pi0Compton2008.root");
+	TFile *file1 = new TFile(filename);
+	TTree *tree1 = (TTree*)file1->Get("Pi0ComptonTree");
+
+	gStyle->SetOptStat(1111);
+
+	Int_t NPromptPi0, NRandomPi0;
+
+	Int_t TaggerChannelPromptPi0[1000], TaggerChannelRandomPi0[1000];
+
+	Double_t MissingMassPromptPi0[1000], MissingMassRandomPi0[1000];
+
+	tree1->SetBranchAddress("NPromptPi0",		&NPromptPi0);
+	tree1->SetBranchAddress("NRandomPi0",		&NRandomPi0);
+
+	tree1->SetBranchAddress("TaggerChannelPromptPi0",&TaggerChannelPromptPi0);
+	tree1->SetBranchAddress("TaggerChannelRandomPi0",&TaggerChannelRandomPi0);
+
+	tree1->SetBranchAddress("MissingMassPromptPi0",	&MissingMassPromptPi0);
+	tree1->SetBranchAddress("MissingMassRandomPi0",	&MissingMassRandomPi0);
+
+	TH1D *a0 = new TH1D("Missing-Mass-Pi0-Prompt-2008","Missing-Mass-Pi0-Prompt-2008", 400,800,1200);
+	TH1D *a1 = new TH1D("Missing-Mass-Pi0-Random-2008","Missing-Mass-Pi0-Random-2008", 400,800,1200);
+	TH1D *a2 = new TH1D("Missing-Mass-Pi0-2008",	   "Missing-Mass-Pi0-2008",	   400,800,1200);
+
+	TH1D *b0 = new TH1D("Missing-Mass-Pi0-Prompt-2012","Missing-Mass-Pi0-Prompt-2012", 400,800,1200);
+	TH1D *b1 = new TH1D("Missing-Mass-Pi0-Random-2012","Missing-Mass-Pi0-Random-2012", 400,800,1200);
+	TH1D *b2 = new TH1D("Missing-Mass-Pi0-2012",	      "Missing-Mass-Pi0-2012",	 400,800,1200);
+
+	Int_t nentries = (Int_t)tree1->GetEntries();
+	for (Int_t i=0;i<nentries;i++) {
+
+		tree1->GetEntry(i);
+		
+		for (Int_t a = 0; a < NPromptPi0; a++){
+			if (TaggerChannelPromptPi0[a] >= 240 && TaggerChannelPromptPi0[a] <= 260){
+				a0->Fill(MissingMassPromptPi0[a]);
+			}
+		}	
+
+		for (Int_t a = 0; a < NRandomPi0; a++){
+			if (TaggerChannelRandomPi0[a] >= 240 && TaggerChannelRandomPi0[a] <= 260){
+				a1->Fill(MissingMassRandomPi0[a]);
+			}	
+		}
+	}
+	
+	Char_t* filename = Form("/work0/cristina/TA2Pi0Compton2012-373.root");
+	TFile *file2 = new TFile(filename);
+	TTree *tree2 = (TTree*)file2->Get("Pi0ComptonTree");
+
+	tree2->SetBranchAddress("NPromptPi0",		&NPromptPi0);
+	tree2->SetBranchAddress("NRandomPi0",		&NRandomPi0);
+
+	tree2->SetBranchAddress("TaggerChannelPromptPi0",&TaggerChannelPromptPi0);
+	tree2->SetBranchAddress("TaggerChannelRandomPi0",&TaggerChannelRandomPi0);
+
+	tree2->SetBranchAddress("MissingMassPromptPi0",	&MissingMassPromptPi0);
+	tree2->SetBranchAddress("MissingMassRandomPi0",	&MissingMassRandomPi0);
+
+	Int_t nentries = (Int_t)tree2->GetEntries();
+	for (Int_t i=0;i<nentries;i++) {
+
+		tree2->GetEntry(i);
+		
+		for (Int_t a = 0; a < NPromptPi0; a++){
+			if (TaggerChannelPromptPi0[a] >= 240 && TaggerChannelPromptPi0[a] <= 260){
+				b0->Fill(MissingMassPromptPi0[a]);
+			}
+		}	
+
+		for (Int_t a = 0; a < NRandomPi0; a++){
+			if (TaggerChannelRandomPi0[a] >= 240 && TaggerChannelRandomPi0[a] <= 260){
+				b1->Fill(MissingMassRandomPi0[a]);
+			}	
+		}
+	}
+
 	canvas1 = new TCanvas("canvas1","Missing-Mass-Compton");
 	a2->Add(a0,1);
 	a2->Add(a1,-0.025); //Data2012
